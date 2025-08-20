ebae1b10ca690b4661844be1c663ea0cac0c4d5b
Florian Sowade <f.sowade@suora.com>
2025-08-20 23:12:08 +0200

    We use the new math, now

--- src/sample.test.ts
+++ src/sample.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 
-test("adds 1 + 2 to equal 3", () => {
+test("adds 1 + 2 to equal 4", () => {
   const sum = 1 + 2;
-  expect(sum).toBe(3);
+  expect(sum).toBe(4);
 });
